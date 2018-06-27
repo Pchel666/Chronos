468928eb6111de6fc4ff22248430be5bbfe80459
Pchel666 <pchel66694@gmail.com>
2018-06-27 14:13:51 -0400

    some changes to login activity

---
+++ app/src/main/java/edu/wit/mobileapp/chronos/WebDummy.java
@@ -0,0 +1,29 @@
+package edu.wit.mobileapp.chronos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebDummy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_dummy);
+        WebView webview = (WebView)findViewById(R.id.webView1);
+        webview.setWebViewClient(new WebViewClient());
+
+        // Enable JavaScript
+        webview.getSettings().setJavaScriptEnabled(true);
+
+        // Zoom out the page to fit the display
+        webview.getSettings().setLoadWithOverviewMode(true);
+        webview.getSettings().setUseWideViewPort(true);
+
+        // Provide pinch zoom operation
+        webview.getSettings().setBuiltInZoomControls(true);
+
+        webview.loadUrl("http://www.lconnect.wit.edu");
+    }
+}
