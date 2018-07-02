cca5a3d3b83d2192ff7292b15f57b9fe5ddbfaa2
Kenny Robinson <robinsonk2@wit.edu>
2018-07-01 21:18:07 -0400

    Data for all courses can now be pulled from all courses. However, the current implemention sucks alot. Unless we can figure out how to automate the clicking of the button, the user will have to click the button a 2*'number of lectures' times.
    Course Info can be sent between activities, currently working on a fix
    to send the lecture times of each course

--- app/src/main/java/edu/wit/mobileapp/chronos/PortraitSchedule.java
+++ app/src/main/java/edu/wit/mobileapp/chronos/PortraitSchedule.java
@@ -3,11 +3,27 @@ package edu.wit.mobileapp.chronos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PortraitSchedule extends AppCompatActivity {
+    List<Course> courseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portrait_schedule);
+        courseList = new ArrayList<>();
+
+        int numCourses = getIntent().getIntExtra("numCourses",0);
+
+        for(int i = 0; i< numCourses; i++){
+            Course courseToAdd = (Course) getIntent().getParcelableExtra("course"+i);
+            courseList.add(courseToAdd);
+        }
+
+        System.out.print("break"); //To set a break point to check data
+
+
     }
 }
--- app/src/main/java/edu/wit/mobileapp/chronos/WebDummy.java
+++ app/src/main/java/edu/wit/mobileapp/chronos/WebDummy.java
@@ -1,6 +1,7 @@
 package edu.wit.mobileapp.chronos;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.PatternMatcher;
 import android.support.v7.app.AppCompatActivity;
@@ -18,15 +19,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class WebDummy extends AppCompatActivity {
 
     private final String TAG = "myApp";
-    int click = 0;
+
+    int click = -1;
     int page = 0;
+
     WebView webview;
     Button myButton;
+
+    List<String> coursesRead;
     Map<String, Course> courses;
+    List<String> links;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,8 @@ public class WebDummy extends AppCompatActivity {
         final Context contextToPass = this;
 
         courses = new HashMap<String, Course>();
+        coursesRead = new ArrayList<>();
+        links = new ArrayList<>();
 
         // Enable JavaScript
         webview.getSettings().setJavaScriptEnabled(true);
@@ -56,23 +65,41 @@ public class WebDummy extends AppCompatActivity {
 
         // Button that uses JavaScript to scrape html data from the webview's current page
         myButton = findViewById(R.id.button);
+        myButton.setText("Count MeetingTimes");
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //Week at a glance page doesn't have course name, and format isn't formatted right to just read from there
-                //Currently only gets first link's course details
-                //TODO: implement navigation to ALL details pages
+                //Week at a glance page doesn't have course name, and not all rows have 7 columns
+                // Gets data from course details pages
                 if(page==0) {
-                    //follow the link to course details page
-                    webview.loadUrl(
-                            "javascript:window.INTERFACE.processContent(document.getElementsByTagName('body')[0]" +
-                                    ".getElementsByTagName('table')[7].getElementsByTagName('tr')[1]" +
-                                    ".getElementsByTagName('a')[0].click());"
-                    );
-                    myButton.setText("Click to Scrape Details"); // Adapt the button text for the page user is on
-                    page=1; // WebView is now on a Details Page
-                    click++; // Set the next details page to go to
+                    if (click == -1){
+                        webview.loadUrl(
+                                "javascript:window.INTERFACE.processContent(document.getElementsByTagName('body')[0]" +
+                                        ".getElementsByTagName('table')[7].innerHTML);"
+                        );
+                        click=0;
+                    } else if(getLinksLeft()==0) {
+                        Intent dataToSchedule = new Intent();
+                        dataToSchedule.setClass(WebDummy.this, PortraitSchedule.class);
+                        int i=0;
+                        for(Course course:courses.values()){
+                            dataToSchedule.putExtra("course"+i+"",course);
+                            i++;
+
+                        }
+                        dataToSchedule.putExtra("numCourses", courses.size());
+
+                        startActivity(dataToSchedule);
+                    } else {
+                        //follow the link to course details page
+                        webview.loadUrl("https://prodweb2.wit.edu"+links.get(click).replace("amp;",""));
+                        myButton.setText("Click to Scrape Details Page"); // Adapt the button text for the page user is on
+                        page=1; // WebView is now on a Details Page
+                        click++; // Set the next details page to go to
+
+                    }
+
 
                 } else if (page == 1) {
                     // get info from courseDetailsPage
@@ -85,11 +112,19 @@ public class WebDummy extends AppCompatActivity {
                             "javascript:window.history.back();"
                     );
 
-                    page=0; // WebView is now on week at a glance Page
-                    myButton.setText("SCRAPE (ONLY ON CORRECT PAGE)");   // Adapt the button text for the page user is on
+                    // Adapt the button text for the page user is on, according to how many links are left to scrape
+                    if (getLinksLeft()>0){
+                        myButton.setText(String.format("SCRAPE NEXT LINK (%d Remaining)", getLinksLeft()));
+                    } else {
+                        myButton.setText("Click to Import Courses To Chronos");
+                    }
                 }
 
 
+            }
+
+            int getLinksLeft() {
+                return links.size()-click;
             }
 
         });
@@ -104,8 +139,12 @@ public class WebDummy extends AppCompatActivity {
     // An instance of this class will be registered as a JavaScript interface
     class MyJavaScriptInterface
     {
+
+        //Pattern to find the links to various Details Pages
+        Pattern linkPattern = Pattern.compile("<a href=\"(.*)\">");
+
         //Patterns to match to the scraped HTML
-        Pattern titlePattern = Pattern.compile("<caption class=\"captiontext\">(.*)-(.*)-.*</caption>");
+        Pattern titlePattern = Pattern.compile("<caption class=\"captiontext\">(.*)-(.*)-(.*)</caption>");
         Pattern detailsPattern = Pattern.compile("<td class=\"dddefault\">(.*)</td>");
 
 
@@ -117,57 +156,75 @@ public class WebDummy extends AppCompatActivity {
         @JavascriptInterface
         public void processContent(String aContent)
         {
-            // only gets data if page has data
+            // only gets data if page has data to get
             if(!aContent.equals("undefined")) {
 
-                String courseName = "";
-                String courseId = "";
 
-                // Matches courseTitle and courseNumber
-                Matcher titleMatcher = titlePattern.matcher(aContent);
-                if(titleMatcher.find() && titleMatcher.groupCount()==2) {
-                    courseName = titleMatcher.group(1);
-                    courseId = titleMatcher.group(2);
-                }
+                if(page == 0){  //process the request to count the links
 
-                // Matches Meeting Time Details
-                List<String> details = new ArrayList<>();
-                Matcher detailsMatcher = detailsPattern.matcher(aContent);
-                do{
-                    if(detailsMatcher.find() && detailsMatcher.groupCount()==1) {
-                        details.add(detailsMatcher.group(1));
+                    // Add links to the links list
+                    Matcher linkMatcher = linkPattern.matcher(aContent);
+                    do {
+                        if (linkMatcher.find() && linkMatcher.groupCount() == 1) {
+                            links.add(linkMatcher.group(1));
+                        }
+                    } while (!linkMatcher.hitEnd());
+
+                    myButton.setText(String.format("SCRAPE NEXT LINK (%d Remaining)", links.size()));
+                } else { //process the request to get course details
+
+                    String courseName = "";
+                    String courseNumber = "";
+                    String courseId = "";
+
+                    // Matches courseTitle and courseNumber
+                    Matcher titleMatcher = titlePattern.matcher(aContent);
+                    if (titleMatcher.find() && titleMatcher.groupCount() == 3) {
+                        courseName = titleMatcher.group(1);
+                        courseNumber = titleMatcher.group(2);
+                        courseId = titleMatcher.group(2)+titleMatcher.group(3);
                     }
-                } while(!detailsMatcher.hitEnd());
+
+                    // Matches Meeting Time Details
+                    List<String> details = new ArrayList<>();
+                    Matcher detailsMatcher = detailsPattern.matcher(aContent);
+                    do {
+                        if (detailsMatcher.find() && detailsMatcher.groupCount() == 1) {
+                            details.add(detailsMatcher.group(1));
+                        }
+                    } while (!detailsMatcher.hitEnd());
 
 
+                    Log.v(TAG, aContent);
 
-                Log.v(TAG, aContent);
 
+                    // Creates a course to add to the schedule
+                    Course courseToAdd;
+                    if (!courses.isEmpty() && courses.containsKey(courseNumber) && !coursesRead.contains(courseId)) {
+                        courseToAdd = courses.get(courseNumber);
+                    } else {
+                        courseToAdd = new Course(courseNumber);
+                        courseToAdd.courseName = courseName;
+                        courseToAdd.instructor = details.get(13).split("\\(<abbr")[0];
+                    }
 
-                // Creates a course to add to the schedule
-                Course courseToAdd;
-                if (!courses.isEmpty() && courses.containsKey(courseId)) {
-                    courseToAdd = courses.get(courseId);
-                } else {
-                    courseToAdd = new Course(courseId);
-                    courseToAdd.courseName = courseName;
-                    courseToAdd.instructor = details.get(13).split("\\(<abbr")[0];
-                }
+                    // Adds meeting times to course
+                    for (int i = 0; i < details.get(9).length(); i++) {
+                        Lecture lectureToAdd = new Lecture();
+                        lectureToAdd.day = details.get(9).charAt(i);
+                        lectureToAdd.startTime = details.get(8).split("-")[0];
+                        lectureToAdd.endTime = details.get(8).split("-")[1];
+                        lectureToAdd.place = details.get(10);
+                        courseToAdd.addLecture(lectureToAdd);
+                    }
 
-                // Adds meeting times to course
-                for (int i = 0; i < details.get(9).length(); i++) {
-                    Lecture lectureToAdd = new Lecture();
-                    lectureToAdd.day = details.get(9).charAt(i);
-                    lectureToAdd.startTime = details.get(8).split("-")[0];
-                    lectureToAdd.endTime = details.get(8).split("-")[1];
-                    lectureToAdd.place = details.get(10);
-                    courseToAdd.addLecture(lectureToAdd);
-                }
-
-                // if the course already existed in the courses Map, the course has just been updated
-                // otherwise, the course will be added to the courses Map here
-                if (!courses.containsKey(courseId)) {
-                    courses.put(courseId, courseToAdd);
+                    // if the course already existed in the courses Map, the course has just been updated
+                    // otherwise, the course will be added to the courses Map here
+                    if (!courses.containsKey(courseNumber)) {
+                        courses.put(courseNumber, courseToAdd);
+                    }
+                    coursesRead.add(courseId);
+                    page=0;
                 }
             }
         }
