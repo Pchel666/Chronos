231553e6ec368f3b1b93d14c6de9f553a2269cad
Kenny Robinson <robinsonk2@wit.edu>
2018-07-19 10:54:15 -0400

    Data is now available in the landscape schedule

--- app/src/main/java/edu/wit/mobileapp/chronos/LandscapeSchedule.java
+++ app/src/main/java/edu/wit/mobileapp/chronos/LandscapeSchedule.java
@@ -1,13 +1,59 @@
 package edu.wit.mobileapp.chronos;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.gson.Gson;
+
+import java.util.List;
+import java.util.Map;
+
 public class LandscapeSchedule extends AppCompatActivity {
+
+    Map<String,Course> courses;
+    List<Lecture> meetingTimes;
+    private static final String PREFS_NAME = "edu.wit.mobileapp.chronos.PortraitSchedule";
+    /** This application's preferences */
+    private static SharedPreferences saved;
+
+    /** This application's settings editor*/
+    private static SharedPreferences.Editor editor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landscape_schedule);
+
+        //Initilizes gson, savedPreferences and savedPreferences.Editor
+        //Used for storing data to the device for future runs of application
+        Gson gson = new Gson();
+        if(saved == null){
+            saved = this.getApplicationContext().getSharedPreferences(PREFS_NAME,
+                    Context.MODE_PRIVATE );
+        }
+        editor = saved.edit();
+
+        //Try loading existing JSON data from device, and unwrapping it back into the respective List/Map
+        if(saved.contains("coursesJSON")){
+            String existingCourses = saved.getString("coursesJSON","");
+            courses = gson.fromJson(existingCourses, courses.getClass());
+        }
+        if(saved.contains("meetingTimesJSON")){
+            String existingMeetingTimes = saved.getString("meetingTimesJSON", "");
+            meetingTimes = gson.fromJson(existingMeetingTimes, meetingTimes.getClass());
+
+        }
+
+        fillSchedule();
+    }
+
+    private void fillSchedule(){
+        //TODO: uses courses and meeting times to fill the schedule interface
+
+        // break point to check data
+        System.out.print("break");
     }
 }
